440f038b1be343e28e885a1a116f14f9f3d46cef
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-10-28 08:12:56 +0100

    Add stylesheet

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from blueprints import resources
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(
+        __name__,
+        static_folder='static',
+        instance_relative_config=True
+    )
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
