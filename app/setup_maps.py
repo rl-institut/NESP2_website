827e8da53a30a70d850da275529d588a7afee9f2
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2019-12-10 17:46:12 +0100

    Update setup_maps.py

--- app/setup_maps.py
+++ app/setup_maps.py
@@ -1,6 +1,8 @@
 import os
 from shutil import copyfile, rmtree
 
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
 # clone the NESP2 repository locally
 branch = 'dev'
 if os.path.exists('NESP2') is False:
@@ -13,7 +15,7 @@ if os.path.exists('NESP2') is False:
 # copy templates
 template_path = os.path.join('NESP2', 'app', 'templates')
 
-new_template_path = os.path.join('templates', 'maps')
+new_template_path = os.path.join('app', 'templates', 'maps')
 
 if os.path.exists(new_template_path) is False:
     os.mkdir(new_template_path)
@@ -24,10 +26,10 @@ for fname in os.listdir(template_path):
 # copy static files
 static_path = os.path.join('NESP2', 'app', 'static')
 
-new_static_path = os.path.join('static')
+new_static_path = os.path.join('app', 'static')
 
-static_types = ['data', 'images', 'css']
-new_static_types = {'data': 'data', 'images': os.path.join('img', 'maps'), 'css': 'css'}
+static_types = ['data', 'images', 'css', 'js']
+new_static_types = {'data': 'data', 'images': os.path.join('img', 'maps'), 'css': 'css', 'js': 'js'}
 
 for static_type in static_types:
     new_static_type = new_static_types[static_type]
