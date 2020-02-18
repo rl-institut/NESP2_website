80ea9b5d64ad41298c108553be7089418336e0f3
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-02-18 18:45:00 +0100

    Add dummy api return structure

--- app/dashboard.py
+++ app/dashboard.py
@@ -23,3 +23,43 @@ CATEGORIES = {
     "Financial": ["totalInvestment", "averagetarif"],
 }
 
+
+VALUES = [{"key": 2017, "value": 1}, {"key": 2018, "value": 2}, {"key": 2019, "value": 3}, {"key": 2020, "value": 4}]
+DUMMY_RETURN = {
+    "peopleConnected": {
+        "unitOfMeasure": "",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+    "communitiesConnected": {
+        "unitOfMeasure": "",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+    "installedRenewableEnergyCapacity": {
+        "unitOfMeasure": "kW",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+    "electricityConsumed": {
+        "unitOfMeasure": "kWh",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+    "totalInvestment": {
+        "unitOfMeasure": "USD",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+    "averagetarif": {
+        "unitOfMeasure": "USD/kWh",
+        "trendValue": 0,
+        "value": 0,
+        "points": VALUES,
+    },
+}
