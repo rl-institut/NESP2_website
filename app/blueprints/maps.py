73630e71dad36b371d44e8129d673a8f73342c00
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-02-21 19:24:41 +0100

    Add default argument to the render_template

--- app/blueprints/maps.py
+++ app/blueprints/maps.py
@@ -6,6 +6,13 @@ bp = Blueprint('maps', __name__)
 @bp.route('/maps/')
 @bp.route('/maps')
 def index():
+    defaultArgs = {
+        "states_content": 1,
+        "grid_content": 1
+    }
+    if request.args == {}:
+        request.args = defaultArgs
+
     try:
         return render_template('maps/index.html', **request.args)
     except TemplateNotFound:
